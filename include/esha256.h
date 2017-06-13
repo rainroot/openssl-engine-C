50fd69cc0303119b98804de00460e4d78b7bd8b9
root <root@localhost.localdomain>
2017-06-13 11:21:00 +0900

    SHA256 header file

--- include/esha256.h
+++ include/esha256.h
@@ -19,9 +19,7 @@
         T1 = X[(i)&0x0f] += s0 + s1 + X[(i+9)&0x0f];    \
         ROUND_00_15(i,a,b,c,d,e,f,g,h);         } while (0)
 
-
 int sha256_init(EVP_MD_CTX *ctx);
 int sha256_update(EVP_MD_CTX *ctx, const void *data_, size_t len);
-//int sha256_update(EVP_MD_CTX *ctx, const void *data, size_t count);
 int sha256_final(EVP_MD_CTX *ctx, unsigned char *md);
 
