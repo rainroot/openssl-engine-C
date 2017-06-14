6171d7c9ccacf8683f431c488f24e6e09150f585
root <root@localhost.localdomain>
2017-06-14 14:25:18 +0900

    AES header file

--- include/eaes.h
+++ include/eaes.h
@@ -1,10 +1,30 @@
-#include <openssl/aes.h>
+#include <stddef.h>
 
+typedef unsigned char u8;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 
-int aes_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc);
-int aes_ecb_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t len);
-int aes_cbc_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t len);
+#define GETU32(p) (*((u32*)(p)))
+#define AES_MAXNR 14
+#define AES_BLOCK_SIZE 16
 
+#define Te0 (u32)((u64*)((u8*)Te+0))
+#define Te1 (u32)((u64*)((u8*)Te+3))
+#define Te2 (u32)((u64*)((u8*)Te+2))
+#define Te3 (u32)((u64*)((u8*)Te+1))
+
+#define Td0 (u32)((u64*)((u8*)Td+0))
+#define Td1 (u32)((u64*)((u8*)Td+3))
+#define Td2 (u32)((u64*)((u8*)Td+2))
+#define Td3 (u32)((u64*)((u8*)Td+1))
+
+# define STRICT_ALIGNMENT 0
+
+struct aes_key_st {
+    unsigned int rd_key[4 * (AES_MAXNR + 1)];
+    int rounds;
+};
+typedef struct aes_key_st AES_KEY;
 
 typedef struct {
     union {
@@ -38,5 +58,13 @@ const EVP_CIPHER *EVP_aes_##keylen##_##mode(void) \
         BLOCK_CIPHER_generic(nid,keylen,1,16,cfb1,cfb1,CFB,flags)       \
         BLOCK_CIPHER_generic(nid,keylen,1,16,cfb8,cfb8,CFB,flags)       \
         BLOCK_CIPHER_generic(nid,keylen,1,16,ctr,ctr,CTR,flags)
+
+int aes_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc);
+int aes_ecb_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t len);
+int aes_cbc_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t len);
+int aes_set_encrypt_key(const unsigned char *userKey, const int bits, AES_KEY *key);
+int aes_set_decrypt_key(const unsigned char *userKey, const int bits, AES_KEY *key);
+void aes_encrypt(const unsigned char *in, unsigned char *out, const AES_KEY *key);
+void aes_decrypt(const unsigned char *in, unsigned char *out, const AES_KEY *key);
 
 
