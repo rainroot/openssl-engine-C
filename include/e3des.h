9d0ab1af78376164539cf774b3be88835659f01b
root <root@localhost.localdomain>
2017-06-22 13:53:59 +0900

    3DES header file

--- include/e3des.h
+++ include/e3des.h
@@ -1,5 +1,3 @@
-#include <openssl/des.h>
-
 typedef struct {
     union {
         double align;
@@ -10,13 +8,13 @@ typedef struct {
                      const DES_key_schedule *, unsigned char *);
     } stream;
 } EOPENSSL_DES_EDE_KEY;
-
-# define ks1 ks.ks[0]
-# define ks2 ks.ks[1]
-# define ks3 ks.ks[2]
-
 # define eopenssl_des3(ctx) ((EOPENSSL_DES_EDE_KEY *)(ctx)->cipher_data)
 
+
 int des_ede3_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc);
+void DES_encrypt2(DES_LONG *data, DES_key_schedule *ks, int enc);
+void DES_encrypt3(DES_LONG *data, DES_key_schedule *ks1, DES_key_schedule *ks2, DES_key_schedule *ks3);
+void DES_decrypt3(DES_LONG *data, DES_key_schedule *ks1, DES_key_schedule *ks2, DES_key_schedule *ks3);
+void DES_ede3_cbc_encrypt(const unsigned char *input, unsigned char *output, long length, DES_key_schedule *ks1, DES_key_schedule *ks2, DES_key_schedule *ks3, DES_cblock *ivec, int enc);
 int des_ede_cbc_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t inl);
 
